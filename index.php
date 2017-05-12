111ef4139321a1edb7412a79736e4170d8e01385
Travis <tkess17@gmail.com>
2017-05-12 11:03:54 -0500

    clean up

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-	//$apiKey = '5e256fa9-68a1-427f-835f-46ce7e1d428e';
+	//$apiKey = '';
 	//$summonerName = 'tkess';
 	
 	//$result = file_get_contents('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' . $summonerName . '?api_key=' . $apiKey);
